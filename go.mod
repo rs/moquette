8d59d67d17d5e87c702f783ab23bfc1f1995b449
Eugene Medvedev <rn3aoh.g@gmail.com>
2018-11-24 15:24:58 +0300

    Feature: Reading a configuration file.
    Probably a very overkill way to do it, but seeing as how I know
    next to no Golang, it's surprising I got it working at all.

--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module github.com/rs/moquette
 
 require (
 	github.com/eclipse/paho.mqtt.golang v0.0.0-20180614102224-88c4622b8e24
+	github.com/spf13/pflag v1.0.3
+	github.com/spf13/viper v1.2.1
 	golang.org/x/net v0.0.0-20180724234803-3673e40ba225 // indirect
-	golang.org/x/sys v0.0.0-20180806192500-2be389f392cd
+	golang.org/x/sys v0.0.0-20180906133057-8cf3aee42992
 )
--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"crypto/tls"
-	"flag"
 	"log"
 	"os"
 	"strconv"
 	"time"
+
+	flag "github.com/spf13/pflag"
+	"github.com/spf13/viper"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/rs/moquette/server"
@@ -14,29 +16,68 @@ import (
 
 func main() {
 	hostname, _ := os.Hostname()
-	debug := flag.Bool("debug", false, "Turn on debugging")
-	broker := flag.String("broker", "tcp://127.0.0.1:1883", "The full url of the mqtt broker to connect to ex: tcp://127.0.0.1:1883")
-	clientID := flag.String("client-id", hostname+strconv.Itoa(time.Now().Second()), "A client id for the connection")
-	username := flag.String("username", "", "A username to authenticate to the mqtt server")
-	password := flag.String("password", "", "Password to match username")
-	confDir := flag.String("conf", "/etc/moquette.d", "Path to the configuration director")
-	sep := flag.String("sep", ":", "File name separator used for topic separator (/)")
-	flag.Parse()
 
-	if *debug {
+	flag.Bool("debug", false, "Turn on debugging")
+	flag.String("broker", "tcp://127.0.0.1:1883", "The full url of the mqtt broker to connect to ex: tcp://127.0.0.1:1883")
+	flag.String("client-id", hostname+strconv.Itoa(time.Now().Second()), "A client id for the connection")
+	flag.String("username", "", "A username to authenticate to the mqtt server")
+	flag.String("password", "", "Password to match username")
+	flag.String("conf", "/etc/moquette.d", "Path to the configuration directory")
+	flag.String("sep", ":", "File name separator used for topic separator (/)")
+	flag.String("configfile", "", "Configuration file name to read options from.")
+
+	flag.Parse()
+	viper.BindPFlags(flag.CommandLine)
+
+	explicitConfig := viper.GetString("configfile")
+
+	if explicitConfig != "" {
+		viper.SetConfigFile(explicitConfig)
+		err := viper.ReadInConfig()
+		if err != nil {
+			log.Fatal("Fatal error when loading config file given on command line:\n", err)
+		}
+	} else {
+		viper.SetConfigName("moquette")
+		viper.AddConfigPath(".")
+		viper.AddConfigPath("$HOME/.local/config/moquette")
+		viper.AddConfigPath("/etc/moquette/")
+		viper.AddConfigPath("/etc/")
+
+		// With implicit config files, it's permissible not to have them at all.
+		err := viper.ReadInConfig()
+
+		if err != nil {
+			if _, cnf := err.(viper.ConfigFileNotFoundError); cnf {
+				log.Print("No config file found, using defaults and command line only.")
+			} else {
+				log.Fatal("Fatal error when loading config file:\n", err)
+			}
+		}
+	}
+
+	debug := viper.GetBool("debug")
+	broker := viper.GetString("broker")
+	clientID := viper.GetString("client-id")
+	username := viper.GetString("username")
+	password := viper.GetString("password")
+	confDir := viper.GetString("conf")
+	sep := viper.GetString("sep")
+
+	if debug {
 		mqtt.DEBUG = log.New(os.Stderr, "", 0)
 	}
 	mqtt.ERROR = log.New(os.Stderr, "", 0)
 
 	connOpts := mqtt.NewClientOptions().
-		AddBroker(*broker).
-		SetClientID(*clientID).
+		AddBroker(broker).
+		SetClientID(clientID).
 		SetCleanSession(true).
 		SetKeepAlive(2 * time.Second)
-	if *username != "" {
-		connOpts.SetUsername(*username)
-		if *password != "" {
-			connOpts.SetPassword(*password)
+	if username != "" {
+		connOpts.SetUsername(username)
+		if password != "" {
+			connOpts.SetPassword(password)
 		}
 	}
 	tlsConfig := &tls.Config{InsecureSkipVerify: true, ClientAuth: tls.NoClientCert}
@@ -46,7 +87,7 @@ func main() {
 		log.Print("Connected")
 	})
 
-	s := server.New(connOpts, *confDir, *sep)
+	s := server.New(connOpts, confDir, sep)
 	stop := make(chan struct{})
 	if err := s.Run(stop); err != nil {
 		panic(err)
